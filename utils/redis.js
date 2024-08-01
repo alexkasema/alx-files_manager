4e9297bad3cb3155d8b4a5c5eae2115cfaa810d1
Alex Kasema <lexxmulwa@gmail.com>
2024-08-01 11:42:58 +0000

    Created a class that Instantiates Redis and performs redis client operations.

--- utils/redis.js
+++ utils/redis.js
@@ -9,7 +9,7 @@ class RedisClient {
    * Instantiate a new redis client
    */
 
-  constructor () {
+  constructor() {
     this.client = createClient();
     this.getAsyncFunc = promisify(this.client.get).bind(this.client);
 
@@ -27,14 +27,14 @@ class RedisClient {
   /**
    * Checks if the connection to redis was a success
    */
-  isAlive () {
+  isAlive() {
     return this.isClientConnected;
   }
 
   /**
    * Gets the value associated with the key in redis
    */
-  async get (key) {
+  async get(key) {
     const value = await this.getAsyncFunc(key);
     return value;
   }
@@ -42,7 +42,7 @@ class RedisClient {
   /**
    * Creates a new key, value pair in redis with an expiration time
    */
-  async set (key, value, duration) {
+  async set(key, value, duration) {
     await promisify(this.client.setex)
       .bind(this.client)(key, duration, value);
   }
@@ -50,7 +50,7 @@ class RedisClient {
   /**
    * Removes the value associated with the given key from redis
    */
-  async del (key) {
+  async del(key) {
     await promisify(this.client.del).bind(this.client)(key);
   }
 }
